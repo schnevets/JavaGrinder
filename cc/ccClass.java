f10c912c12eb30907a44b7e63de824b6aad0345b
danieluebelein <danieluebelein@172-26-206-92.dynapool.nyu.edu>
2011-11-05 16:01:01 -0400

    cc class and method classes somewhat fleshed out. somewhat.

--- cc/ccClass.java
+++ cc/ccClass.java
@@ -1,7 +1,67 @@
 package oop;
 
+import java.util.ArrayList;
+import oop.ccMethod;
+
 public class ccClass {
-	public ccClass(){
-		
+	private final String name;
+	private final String access;
+	private final boolean isStatic;
+	private ArrayList<ccMethod> methods;
+	
+	public ccClass(String clName, String clAccess){
+		name = clName;
+		access = clAccess;
+		isStatic = false;
+		methods = new ArrayList<ccMethod>();
 	}
+	public ccClass(String clName, String clAccess, boolean clIsStatic){
+		name = clName;
+		access = clAccess;
+		isStatic = clIsStatic;
+		methods = new ArrayList<ccMethod>();
+	}
+	
+	/** Adds method to class's list of methods */
+	public void addMethod(ccMethod method){
+		methods.add(method);
+	}
+	
+	/** Gets the name of the class. Of course. */
+	public String getName(){
+		return name;
+	}
+	/** Gets the access level of the class (Public, Private, Protected) */
+	public String getAccess(){
+		return access;
+	}
+	/** Static? (y/n) */
+	public boolean getIsStatic(){
+		return isStatic;
+	}
+	/**
+	 * Searches for and returns method named s.
+	 * If the desired method is not in the list of methods, it returns a dummy method with the name "METHOD NOT FOUND".
+	 * 
+	 * @param s Name of desired method.
+	 * @return The method with name s.
+	 */
+	public ccMethod getMethod(String s){
+		for(int i = 0; i < methods.size(); i++){
+			if(methods.get(i).getName() == s)
+				return methods.get(i);
+		}
+		return new ccMethod("METHOD NOT FOUND", "", "", new String[0]);
+	}
+	/**
+	 * If you know the index of the method you want, somehow, then this is a marginally faster way to get it.
+	 * 
+	 * @param i Index of desired method.
+	 * @return The method at index i.
+	 */
+	public ccMethod getMethodAtIndex(int i){
+		return methods.get(i);
+	}
+	
+	
 }
--- cc/ccMethod.java
+++ cc/ccMethod.java
@@ -1,7 +1,31 @@
 package oop;
 
 public class ccMethod {
-	public ccMethod(){
-		
+	private final String name;
+	private final String access;
+	private final String returnType;
+	private final String[] argumentType;
+	private final boolean isStatic;
+	
+	public ccMethod(String mName, String mAccess, String mReturnType, String[] mArgumentType){
+		name = mName;
+		access = mAccess;
+		returnType = mReturnType;
+		argumentType = mArgumentType;
+		isStatic = false;
 	}
+	public ccMethod(String mName, String mAccess, String mReturnType, String[] mArgumentType, boolean mIsStatic){
+		name = mName;
+		access = mAccess;
+		returnType = mReturnType;
+		argumentType = mArgumentType;
+		isStatic = mIsStatic;
+	}
+
+	public String getName(){
+		return name;
+	}
+	
+	
+	
 }
